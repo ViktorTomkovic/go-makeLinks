850a40ec6dfeda9cbc3ba2b51494f21dc4f97529
Viktor Tomkovič <viktor.tomkovic@gmail.com>
2024-04-01 20:09:37 +0200

    Symlinks are deleted, files & folders renamed, then new symlinks are created

--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io/fs"
 	"os"
-	"path"
 	"path/filepath"
+	"strings"
 )
 
 // thank you https://stackoverflow.com/a/61837617
 type StringSlice []string
 
 func (ss *StringSlice) UnmarshalJSON(data []byte) error {
+	fmt.Println(string(data))
 	if data[0] == '[' {
 		return json.Unmarshal(data, (*[]string)(ss))
 	} else if data[0] == '"' {
@@ -25,33 +28,67 @@ func (ss *StringSlice) UnmarshalJSON(data []byte) error {
 }
 
 func main() {
-	ex, err := os.Executable()
-	if err != nil {
-		panic(err)
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s linkFile\n", os.Args[0])
+		return
 	}
-	exPath := filepath.Dir(ex)
-	fmt.Println(exPath)
 	wd, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(wd)
-	if len(os.Args) < 2 {
-		fmt.Printf("Usage: %s linkFile", os.Args[0])
-		return
-	}
+	fmt.Printf("Working directory: %s\n", wd)
 	sourceFile := os.Args[1]
-	fmt.Println(sourceFile)
-	sourcePath := path.Join(wd, sourceFile)
-	fmt.Println(sourcePath)
-	sourceBytes, err := os.ReadFile(sourcePath)
-	fmt.Println(string(sourceBytes))
+	sourceBytes, err := os.ReadFile(sourceFile)
 	var linkMap map[string]StringSlice
 	err = json.Unmarshal([]byte(sourceBytes), &linkMap)
 	if err != nil {
 		panic(err)
 	}
-	for key, value := range linkMap {
-		fmt.Printf("%s %s\n", key, value)
+	home, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	for target, links := range linkMap {
+		fmt.Printf("%s %s\n", target, links)
+		target = filepath.Join(wd, target)
+		for _, link := range links {
+			if strings.HasPrefix(link, "~/") {
+				link = filepath.Join(home, link[2:])
+			}
+			lstat, err := os.Lstat(link)
+			if errors.Is(err, os.ErrNotExist) {
+				fmt.Println("Link location doesn't exist.")
+				dir := filepath.Dir(link)
+				err := os.MkdirAll(dir, os.FileMode(0755))
+				if err != nil {
+					panic(err)
+				}
+			} else if err != nil {
+				panic(err)
+			} else {
+				// Assuming err is nil and lstat is not nil
+				//fmt.Printf("%+v\n", lstat)
+				if lstat.Mode().Type() == fs.ModeSymlink {
+					fmt.Printf("Removing symlink '%s'\n", link)
+					err := os.Remove(link)
+					if err != nil {
+						panic(err)
+					}
+				}
+				if lstat.Mode().IsDir() || lstat.Mode().IsRegular() {
+					fmt.Printf("Moving '%s' -> '%s'", link, link+".bak\n")
+					err = os.Rename(link, link+".bak")
+					if err != nil {
+						panic(err)
+					}
+				}
+			}
+			fmt.Printf("Creating link '%s' -> '%s'\n", link, target)
+			err = os.Symlink(target, link)
+			if err != nil {
+				fmt.Printf("%+v\n", err)
+			}
+		}
 	}
 }
+
